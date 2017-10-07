f9f5980d392f407d576e2bb744e140a418667c7d
Vuk Bojanic <vukgraphicart@gmail.com>
2017-10-07 11:42:42 +0200

    Post Loop

--- header.php
+++ header.php
@@ -4,8 +4,16 @@
 		<meta charset="utf-8">
 		<title>Kinetic Theme</title>
 		<?php wp_head(); ?>
-	</head>
+  </head>
+  
+  <?php 
+    if(is_front_page()):
+      $kinetic_classes = array('kinetic-class', 'my-class');
+    else:
+      $kinetic_classes = array('no-kinetic-class');
+    endif;
+  ?>
 	
-	<body>
+	<body <?php body_class($kinetic_classes); ?>>
 
       <?php wp_nav_menu(array('theme_location'=>'primary')); ?>
